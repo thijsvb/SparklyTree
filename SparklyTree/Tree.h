70a8b3a1603ef20d46fe4ee6bf8702f6bb596b0b
Thijs <thijsvb@gmail.com>
2016-12-16 12:39:12 +0100

    Update Tree.h

--- SparklyTree/Tree.h
+++ SparklyTree/Tree.h
@@ -1,11 +1,14 @@
 #ifndef Tree_h
 #define Tree_h
+#include <NeoPixelBus.h>
+#include <NeoPixelAnimator.h>
 
 #include "Arduino.h"
 
 class Tree {
   public:
     Tree(int w, int h, int p);
+    NeoPixelBus lights;
     int width, height, nLights;
     int lightAt(int l, int n);
     void set(int l, int n, int r, int g, int b);
