15da1acbeec1d5daa67ad8cab9923153f18d44a9
thijvb <thijsvb@gmail.com>
2017-12-19 15:44:48 +0100

    changed to an easier approach, because time :(

--- SparklyTree/SparklyTree.ino
+++ SparklyTree/SparklyTree.ino
@@ -1,15 +1,35 @@
-#include <NeoPixelBus.h>
-#include <NeoPixelAnimator.h>
-#include <Tree.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
 
-Tree tree;
-const int treePin = 4;
+const int stripPin = 9;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, stripPin, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  tree = new Tree(10, 10, treePin);
+  strip.begin();
+  strip.show();
+  Serial.begin(9600);
 }
 
+int j = 0;
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for(int i=0; i!=strip.numPixels(); ++i) {
+    uint32_t c;
+    if((i+j)%3 == 0) {
+      c = strip.Color(255, 0, 0);
+    }
+    else if((i+j)%3 == 1) {
+      c = strip.Color(0, 255, 0);
+    }
+    else if((i+j)%3 == 2) {
+      c = strip.Color(0, 0, 255);
+    }
+    strip.setPixelColor(i, c);
+  }
+  strip.show();
+  ++j;
+  delay(100);
 }
+
